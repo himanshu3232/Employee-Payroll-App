2cfb4809dcf2f6bf6200f23256857bcfa2415754
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-24 10:48:09 +0530

    [Himanshu Ranjan] init

---
+++ payroll-app/src/test/java/com/capgemini/payroll_app/PayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.payroll_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
